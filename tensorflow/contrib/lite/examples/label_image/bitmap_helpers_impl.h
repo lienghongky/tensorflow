00ec4ebae3376f9d04c49d625b31deec6a2b010a
Koan-Sin Tan <koansin.tan@gmail.com>
2018-01-22 21:09:21 +0800

    test bilinear
    doesn't work yet

--- tensorflow/contrib/lite/examples/label_image/bitmap_helpers_impl.h
+++ tensorflow/contrib/lite/examples/label_image/bitmap_helpers_impl.h
@@ -16,13 +16,27 @@ limitations under the License.
 #ifndef TENSORFLOW_CONTRIB_LITE_EXAMPLES_LABEL_IMAGE_BITMAP_HELPERS_IMPL_H
 #define TENSORFLOW_CONTRIB_LITE_EXAMPLES_LABEL_IMAGE_BITMAP_HELPERS_IMPL_H
 
+#include "tensorflow/contrib/lite/builtin_op_data.h"
+#include "tensorflow/contrib/lite/interpreter.h"
+#include "tensorflow/contrib/lite/kernels/register.h"
+#include "tensorflow/contrib/lite/string_util.h"
+#include "tensorflow/contrib/lite/version.h"
+#include "tensorflow/contrib/lite/model.h"
+
+#include <fcntl.h>      // NOLINT(build/include_order)
+#include <getopt.h>     // NOLINT(build/include_order)
+#include <sys/time.h>   // NOLINT(build/include_order)
+#include <sys/types.h>  // NOLINT(build/include_order)
+#include <sys/uio.h>    // NOLINT(build/include_order)
+#include <unistd.h>     // NOLINT(build/include_order)
+
 #include "tensorflow/contrib/lite/examples/label_image/label_image.h"
 
 namespace tflite {
 namespace label_image {
 
 template <class T>
-void downsize(T* out, uint8_t* in, int image_height, int image_width,
+void downsize_old(T* out, uint8_t* in, int image_height, int image_width,
               int image_channels, int wanted_height, int wanted_width,
               int wanted_channels, Settings* s) {
   for (int y = 0; y < wanted_height; ++y) {
@@ -41,6 +55,77 @@ void downsize(T* out, uint8_t* in, int image_height, int image_width,
       }
     }
   }
+}
+
+template <class T>
+void downsize(T* out, uint8_t* in, int image_height, int image_width,
+              int image_channels, int wanted_height, int wanted_width,
+              int wanted_channels, Settings* s) {
+
+  int fd = open("/tmp/in.rgb", O_CREAT|O_RDWR); 
+  write(fd, in, image_height*image_width*image_channels);
+  close(fd);
+
+  float *fin = new float[image_height * image_width * image_channels];
+
+  for (int i=0; i < image_height * image_width * image_channels; i++)
+    fin[i] = in[i];
+
+  tflite::Interpreter *bar = new Interpreter();
+  int base_index = 0;
+
+  // one input
+  bar->AddTensors(1, &base_index);
+  // one output
+  bar->AddTensors(1, &base_index);
+  // LOG(INFO) << "tensors: " << bar->tensors_size() << "\n";
+  bar->SetInputs({0});
+  bar->SetOutputs({1});
+
+  TfLiteQuantizationParams quant;
+
+  bar->SetTensorParametersReadWrite(0, kTfLiteFloat32, "input", {1, image_height, image_width, image_channels}, quant);
+  bar->SetTensorParametersReadWrite(1, kTfLiteFloat32, "output", {1, wanted_height, wanted_width, wanted_channels}, quant);
+
+  tflite::ops::builtin::BuiltinOpResolver resolver;
+  TfLiteRegistration *resize = resolver.FindOp(BuiltinOperator_RESIZE_BILINEAR);
+  TfLiteResizeBilinearParams wh = {wanted_height, wanted_width};
+  printf("%d, %d\n", wh.new_height, wh.new_width);
+  bar->AddNodeWithParameters({0}, {1}, nullptr, 0, &wh, resize, nullptr);
+
+  TfLiteIntArray* dims = bar->tensor(1)->dims;
+  printf("%s: %d, %d\n", bar->tensor(1)->name, bar->tensor(1)->bytes, bar->tensor(1)->dims->size);
+  printf("%d, %d, %d, %d\n", dims->data[0], dims->data[1], dims->data[2], dims->data[3]);
+
+  bar->AllocateTensors();
+  bar->typed_tensor<float*>(0)[0] = fin;
+  bar->UseNNAPI(true);
+
+  bar->Invoke();
+
+  printf("%s: %d, %d\n", bar->tensor(1)->name, bar->tensor(1)->bytes, bar->tensor(1)->dims->size);
+  dims = bar->tensor(1)->dims;
+  printf("%d, %d, %d, %d\n", dims->data[0], dims->data[1], dims->data[2], dims->data[3]);
+
+  float *output = bar->typed_tensor<float*>(1)[0];
+  printf("o: %f, %f, %f\n", output[0], output[1], output[2]);
+
+  for (int i=0; i < 224 * 224 * 3; i++) {
+    if (s->input_floating)
+      out[i] = (output[i] - s->input_mean) / s->input_std;
+    else
+      out[i] = (uint8_t)output[i];
+  }
+  if (s->input_floating)
+    printf("o: %f, %f, %f\n", out[0], out[1], out[2]);
+  else  {
+    printf("o: %d, %d, %d\n", out[0], out[1], out[2]);
+    printf("o: %d, %d, %d\n", out[0+224*3], out[1+224*3], out[2+224*3]);
+    printf("o: %d, %d, %d\n", out[224*224*3-3], out[224*224*3-2], out[224*224*3-1]);
+  }
+  fd = open("/tmp/foo.rgb", O_CREAT|O_RDWR); 
+  printf("len = %zu, %lu\n", bar->tensor(1)->bytes, bar->tensor(1)->bytes/4);
+  write(fd, out, bar->tensor(1)->bytes/4);
 }
 
 }  // namespace label_image
--- tensorflow/contrib/lite/examples/label_image/test_simple.cc
+++ tensorflow/contrib/lite/examples/label_image/test_simple.cc
@@ -16,54 +16,212 @@ limitations under the License.
 #include <cstdarg>
 #include <cstdlib>
 #include <iostream>
+#include <map>
 #include <memory>
 #include <string>
 #include <unordered_set>
 #include <vector>
 
+#include "tensorflow/contrib/lite/builtin_op_data.h"
 #include "tensorflow/contrib/lite/kernels/register.h"
 #include "tensorflow/contrib/lite/model.h"
 #include "tensorflow/contrib/lite/string_util.h"
+#include "tensorflow/contrib/lite/version.h"
+#include "tensorflow/contrib/lite/kernels/register.h"
+
+#include "tensorflow/contrib/lite/examples/label_image/bitmap_helpers.h"
 
 #define LOG(x) std::cerr
 
 namespace tflite {
 namespace test_simple {
 
+struct TensorData {
+  TensorType type;
+  std::vector<int> shape;
+  float min;
+  float max;
+  float scale;
+  int32_t zero_point;
+};
+
 std::unique_ptr<tflite::FlatBufferModel> model;
 std::unique_ptr<tflite::Interpreter> interpreter;
 
+std::map<int, TensorData> tensor_data_;
+std::vector<int32_t> inputs_;
+std::vector<int32_t> outputs_;
+std::vector<flatbuffers::Offset<Tensor>> tensors_;
+std::vector<flatbuffers::Offset<OperatorCode>> opcodes_;
+std::vector<flatbuffers::Offset<Operator>> operators_;
+
+flatbuffers::FlatBufferBuilder builder_;
+
+int AddTensor(TensorData t) {
+  int id = tensors_.size();
+
+  // This is slightly different depending on whether we are adding a
+  // quantized or a regular tensor.
+  bool is_quantized = (t.min != 0 || t.max != 0 || t.scale != 0);
+
+  flatbuffers::Offset<QuantizationParameters> q_params = 0;
+
+  if (is_quantized) {
+    q_params = CreateQuantizationParameters(
+        builder_, /*min=*/0, /*max=*/0, builder_.CreateVector<float>({t.scale}),
+        builder_.CreateVector<int64_t>({t.zero_point}));
+  }
+
+  tensors_.push_back(CreateTensor(builder_, builder_.CreateVector<int>({}),
+                                  t.type, /*buffer=*/0,
+                                  /*name=*/0, q_params));
+
+  tensor_data_[id] = t;
+
+  return id;
+}
+
+int AddInput(const TensorData& t) {
+  int id = AddTensor(t);
+  inputs_.push_back(id);
+  return id;
+}
+
+int AddOutput(const TensorData& t) {
+  int id = AddTensor(t);
+  outputs_.push_back(id);
+  return id;
+}
+
+void SetBuiltinOp(BuiltinOperator type,
+                                 BuiltinOptions builtin_options_type,
+                                 flatbuffers::Offset<void> builtin_options) {
+  opcodes_.push_back(CreateOperatorCode(builder_, type, 0));
+  operators_.push_back(CreateOperator(
+      builder_, /*opcode_index=*/0, builder_.CreateVector<int32_t>(inputs_),
+      builder_.CreateVector<int32_t>(outputs_), builtin_options_type,
+      builtin_options,
+      /*custom_options=*/0, CustomOptionsFormat_FLEXBUFFERS));
+}
+
+
 void InitImpl(const std::string& graph, const std::vector<int>& sizes,
               const std::string& input_layer_type, int num_threads) {
+#if 0
+  auto input1_ = AddInput({TensorType_FLOAT32, {1}, 0, 0, 0, 0});
+  auto input2_ = AddInput({TensorType_FLOAT32, {1}, 0, 0, 0, 0});
+  auto output_ = AddOutput({TensorType_FLOAT32, {1}, 0, 0, 0, 0});
 
-  model = tflite::FlatBufferModel::BuildFromFile(graph.c_str());
-  if (!model) {
-    LOG(FATAL) << "Failed to mmap model " << graph;
-  }
-  LOG(INFO) << "Loaded model " << graph;
-  model->error_reporter();
-  LOG(INFO) << "resolved reporter";
+  LOG(INFO) << "tensors: " << input1_ << ", " << input2_ << ", " << output_ << "\n";
+
+  SetBuiltinOp(BuiltinOperator_ADD, BuiltinOptions_AddOptions,
+  CreateAddOptions(builder_, ActivationFunctionType_NONE).Union());
+
+  // to build interpreter
+  auto opcodes = builder_.CreateVector(opcodes_);
+  auto operators = builder_.CreateVector(operators_);
+  auto tensors = builder_.CreateVector(tensors_);
+  auto inputs = builder_.CreateVector<int32_t>(inputs_);
+  auto outputs = builder_.CreateVector<int32_t>(outputs_);
+
+  std::vector<flatbuffers::Offset<SubGraph>> subgraphs;
+  auto subgraph = CreateSubGraph(builder_, tensors, inputs, outputs, operators);
+  subgraphs.push_back(subgraph);
+  auto subgraphs_flatbuffer = builder_.CreateVector(subgraphs);
+
+  std::vector<flatbuffers::Offset<Buffer>> buffers_vec;
+  auto buffers = builder_.CreateVector(buffers_vec);
+  auto description = builder_.CreateString("programmatic model");
+  builder_.Finish(CreateModel(builder_, TFLITE_SCHEMA_VERSION, opcodes,
+                              subgraphs_flatbuffer, description, buffers));
+
+  auto* smodel = GetModel(builder_.GetBufferPointer());
+
+  std::unique_ptr<tflite::Interpreter> foo;
+  // foo = BuildInterpreter({1, 1});
 
   tflite::ops::builtin::BuiltinOpResolver resolver;
+  tflite::InterpreterBuilder(smodel, resolver)(&foo);
 
-  tflite::InterpreterBuilder(*model, resolver)(&interpreter);
-  if (!interpreter) {
-    LOG(FATAL) << "Failed to construct interpreter";
-  }
+  LOG(INFO) << "before allocate tensors \n";
+  foo->AllocateTensors();
+  LOG(INFO) << "after allocate tensors \n";
+  auto i1 = foo->typed_tensor<float>(input1_);
+  auto i2 = foo->typed_tensor<float>(input2_);
+  i1[0] = 1.02; i2[0] = 2.0;
+#endif
 
-  if (num_threads != -1) {
-    interpreter->SetNumThreads(num_threads);
-  }
+  // std::unique_ptr<tflite::Interpreter> foo = new Interpreter();
+  tflite::Interpreter *foo = new Interpreter();
+  int base_index;
 
-  int input = interpreter->inputs()[0];
+  // two inputs
+  foo->AddTensors(2, &base_index);
+  // one output
+  foo->AddTensors(1, &base_index);
+  LOG(INFO) << "tensors: " << foo->tensors_size() << "\n";
+  foo->SetInputs({0, 1});
+  foo->SetOutputs({2});
+  LOG(INFO) << "tensors: " << foo->tensors_size() << "\n";
 
-  if (input_layer_type != "string") {
-    interpreter->ResizeInputTensor(input, sizes);
-  }
+  TfLiteQuantizationParams quant;
 
-  if (interpreter->AllocateTensors() != kTfLiteOk) {
-    LOG(FATAL) << "Failed to allocate tensors!";
-  }
+  foo->SetTensorParametersReadWrite(0, kTfLiteFloat32, "input1", {1}, quant);
+  foo->SetTensorParametersReadWrite(1, kTfLiteFloat32, "input2", {1}, quant);
+  foo->SetTensorParametersReadWrite(2, kTfLiteFloat32, "output", {1}, quant);
+
+  tflite::ops::builtin::BuiltinOpResolver resolver;
+  TfLiteRegistration *addR = resolver.FindOp(BuiltinOperator_ADD);
+  if (addR != nullptr)
+    LOG(INFO) << "addR: " << addR->builtin_code << "\n";
+  TfLiteAddParams f;
+  foo->AddNodeWithParameters({0,1}, {2}, nullptr, 0, &f, addR, nullptr);
+
+  foo->AllocateTensors();
+
+  foo->typed_tensor<float>(0)[0] = 1.1;
+  foo->typed_tensor<float>(1)[0] = 2.2;
+
+  LOG(INFO) << "before Invoke()\n";
+  foo->Invoke();
+  LOG(INFO) << "after Invoke()\n";
+  auto o = foo->typed_tensor<float>(2);
+  LOG(INFO) << "output: " << o[0] << "\n";
+
+  label_image::Settings s;
+  int image_width = 224;
+  int image_height = 224;
+  int image_channels = 3;
+  uint8_t* in = label_image::read_bmp(s.input_bmp_name, &image_width, &image_height,
+                         &image_channels, &s);
+  LOG(INFO) << "(w, h, c): " << image_width << ", " << image_height << ", " << image_channels << "\n";
+  float *fin = new float[image_width * image_height * image_channels];
+
+  for (int i=0; i < image_width * image_height * image_channels; i++)
+    fin[i] = in[i];
+
+  tflite::Interpreter *bar = new Interpreter();
+  base_index = 0;
+
+  // one input
+  bar->AddTensors(1, &base_index);
+  // one output
+  bar->AddTensors(1, &base_index);
+  LOG(INFO) << "tensors: " << bar->tensors_size() << "\n";
+  bar->SetInputs({0});
+  bar->SetOutputs({1});
+
+  bar->SetTensorParametersReadWrite(0, kTfLiteFloat32, "input", {1, image_width, image_height, image_channels}, quant);
+  bar->SetTensorParametersReadWrite(1, kTfLiteFloat32, "output", {1, 224, 224, image_channels}, quant);
+
+  TfLiteRegistration *resize = resolver.FindOp(BuiltinOperator_RESIZE_BILINEAR);
+
+  TfLiteResizeBilinearParams wh = {224, 224};;
+  bar->AddNodeWithParameters({0}, {1}, nullptr, 0, &wh, resize, nullptr);
+
+  bar->AllocateTensors();
+  bar->typed_tensor<float*>(0)[0] = fin;
+  bar->Invoke();
 }
 
 int Main(int argc, char** argv) {
