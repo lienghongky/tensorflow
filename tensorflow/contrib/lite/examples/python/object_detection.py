b95615fe58357c4c0a2a7c6158063ea02c9ceb15
Koan-Sin Tan <koansin.tan@gmail.com>
2018-06-19 16:12:54 +0800

    add non-maximum suppression and other
    1. add non-maximum suppression
    2. add command line option for maximum number of boxes to show
    3. add command line option for minimum confidence level (in %)

--- tensorflow/contrib/lite/examples/python/object_detection.py
+++ tensorflow/contrib/lite/examples/python/object_detection.py
@@ -77,6 +77,36 @@ def decode_center_size_boxes(locations):
     locations[i][3] = xmax
   return locations
 
+def iou(box_a, box_b):
+  x_a = max(box_a[0], box_b[0])
+  y_a = max(box_a[1], box_b[1])
+  x_b = min(box_a[2], box_b[2])
+  y_b = min(box_a[3], box_b[3])
+
+  intersection_area = (x_b - x_a + 1) * (y_b - y_a + 1)
+
+  box_a_area = (box_a[2] - box_a[0] + 1) * (box_a[3] - box_a[1] + 1)
+  box_b_area = (box_b[2] - box_b[0] + 1) * (box_b[3] - box_b[1] + 1)
+
+  iou = intersection_area / float(box_a_area + box_b_area - intersection_area)
+  return iou
+
+def nms(p, iou_threshold, max_boxes):
+  sorted_p = sorted(p, reverse=True)
+  selected_predictions = []
+  for a in sorted_p:
+    if len(selected_predictions) > max_boxes:
+      break
+    should_select = True
+    for b in selected_predictions:
+      if iou(a[3], b[3]) > iou_threshold:
+        should_select = False
+        break
+    if should_select:
+      selected_predictions.append(a)
+
+  return selected_predictions
+
 if __name__ == "__main__":
   file_name = "/tmp/grace_hopper.bmp"
   model_file = "/tmp/mobilenet_ssd.tflite"
@@ -84,6 +114,8 @@ if __name__ == "__main__":
   box_prior_file = "/tmp/box_priors.txt"
   input_mean = 127.5
   input_std = 127.5
+  min_score = 20.0
+  max_boxes = 10
   floating_model = False
   show_image = False
   alt_output_order = False
@@ -94,7 +126,9 @@ if __name__ == "__main__":
   parser.add_argument("--labels", help="name of file containing labels")
   parser.add_argument("--input_mean", help="input_mean")
   parser.add_argument("--input_std", help="input standard deviation")
-  parser.add_argument("--show_image", help="show_image")
+  parser.add_argument("--min_score", help="show only > min_score")
+  parser.add_argument("--max_boxes", help="max boxes to show")
+  parser.add_argument("--show_image", help="show image")
   parser.add_argument("--alt_output_order", help="alternative output index")
   args = parser.parse_args()
 
@@ -105,9 +139,13 @@ if __name__ == "__main__":
   if args.labels:
     label_file = args.labels
   if args.input_mean:
-    input_mean = args.input_mean
+    input_mean = float(args.input_mean)
   if args.input_std:
-    input_std = args.input_std
+    input_std = float(args.input_std)
+  if args.min_score:
+    min_score = float(args.min_score)
+  if args.max_boxes:
+    max_boxes = int(args.max_boxes)
   if args.show_image:
     show_image = args.show_image
   if args.alt_output_order:
@@ -159,37 +197,41 @@ if __name__ == "__main__":
   output_classes = np.squeeze( \
                      interpreter.get_tensor(output_details[o_index]['index']))
   if not floating_model:
-    p_scale, p_mean = output_details[1]['quantization']
-    o_scale, o_mean = output_details[0]['quantization']
+    p_scale, p_mean = output_details[p_index]['quantization']
+    o_scale, o_mean = output_details[o_index]['quantization']
 
     predictions = (predictions - p_mean * 1.0) * p_scale
     output_classes = (output_classes - o_mean * 1.0) * o_scale
 
   decode_center_size_boxes(predictions)
 
-  heap = []
-  for r in range(0, NUM_RESULTS):
-    top_class_score = -1000.0
-    top_class_score_index = -1
-
-    for c in range(1, NUM_CLASSES):
+  pruned_predictions = [[],]
+  for c in range(1, NUM_CLASSES):
+    pruned_predictions.append([])
+    for r in range(0, NUM_RESULTS):
       score = 1. / (1. + math.exp(-output_classes[r][c]))
-      if score > top_class_score:
-        top_class_score_inxdex = c
-        top_class_score = score
+      if score > 0.01:
+        rect = (predictions[r][1] * width, predictions[r][0] * width, \
+                predictions[r][3] * width, predictions[r][2] * width)
 
-    if top_class_score > 0.001:
-      rect = (predictions[r][1] * width, predictions[r][0] * width, \
-              predictions[r][3] * width, predictions[r][2] * width)
-      heappush(heap, (output_classes[r][top_class_score_inxdex], r, \
-                      labels[top_class_score_inxdex], rect))
+        pruned_predictions[c].append((output_classes[r][c], r, labels[c], rect))
 
-  fig, ax = plt.subplots(1)
+  final_predictions = []
+  for c in range(1, NUM_CLASSES):
+    predictions_for_class = pruned_predictions[c]
+    suppressed_predictions = nms(predictions_for_class, 0.5, max_boxes)
+    final_predictions = final_predictions +  suppressed_predictions
 
-  ten = nlargest(10, heap)
-  for e in ten:
-    score = '{0:2.0f}%'.format(100. / (1. + math.exp(-e[0])))
-    print(score, e[2], e[3])
+  if show_image:
+    fig, ax = plt.subplots(1)
+
+  final_predictions = sorted(final_predictions, reverse=True)[:max_boxes]
+  for e in final_predictions:
+    score = 100. / (1. + math.exp(-e[0]))
+    score_string = '{0:2.0f}%'.format(score)
+    print(score_string, e[2], e[3])
+    if score < min_score:
+      break
     left, top, right, bottom = e[3]
     rect = patches.Rectangle((left, top), (right - left), (bottom - top), \
              linewidth=1, edgecolor='r', facecolor='none')
@@ -197,7 +239,7 @@ if __name__ == "__main__":
     if show_image:
       # Add the patch to the Axes
       ax.add_patch(rect)
-      ax.text(left, top, e[2]+': '+score, fontsize=6,
+      ax.text(left, top, e[2]+': '+score_string, fontsize=6,
               bbox=dict(facecolor='y', edgecolor='y', alpha=0.5))
 
   if show_image:
