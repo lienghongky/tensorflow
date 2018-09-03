abf9104fa5fa4e7efe9fb31ef2b00d4a2e0c46c5
Koan-Sin Tan <koansin.tan@gmail.com>
2018-09-03 17:04:26 +0800

    resnet v2 50 test

---
+++ tensorflow/compiler/aot/tests/my_resnetv2.cc
@@ -0,0 +1,49 @@
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#define EIGEN_USE_THREADS
+#define EIGEN_USE_CUSTOM_THREAD_POOL
+
+#include "tensorflow/compiler/aot/benchmark.h"
+#include "tensorflow/compiler/aot/tests/test_graph_resnetv2.h"
+
+using namespace std;
+using namespace tensorflow::tfcompile;
+
+void get_file_to_float(string fn, float bmp_float[][299][3])
+{
+  ifstream file(fn, ios::in|ios::binary|ios::ate);
+  uint8_t bmp[299][299][3];
+
+  if (file.is_open())
+    file.read((char *)bmp, 299*299*3);
+
+  for (int i; i < 299; i++)
+    for (int j; j < 299; j++)
+      for (int k; k < 3; k++)
+        bmp_float[i][j][j] = (bmp[i][j][k] - 127.5f) / 127.5f;
+}
+
+int main(int argc, char *argv[])
+{
+  string foo = "/tmp/grace_hopper.rgb";
+  float bmp_float[299][299][3];
+
+  get_file_to_float(foo, bmp_float);
+
+  Eigen::ThreadPool tp(4);  // Size the thread pool as appropriate.
+  Eigen::ThreadPoolDevice device(&tp, tp.NumThreads());
+
+  foobar::ResnetV2 mobilenetv1;
+  mobilenetv1.set_thread_pool(&device);
+
+  mobilenetv1.set_arg0_data(bmp_float);
+
+  benchmark::Options options;
+  benchmark::Stats stats;
+  benchmark::Benchmark(options, [&] { mobilenetv1.Run(); }, &stats);
+  benchmark::DumpStatsToStdout(stats);
+
+}
